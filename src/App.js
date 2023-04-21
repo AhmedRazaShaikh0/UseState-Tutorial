a75f8e88f8759ba45d7910073155dd088e06c11d
Ahmed Raza <ahmedraza6100@gmail.com>
2023-04-21 06:05:49 +0500

    use state updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Ahmed() {
+    return (
+        <div>
+            <h1>Ahmed</h1>
+            <h1>Ahmed</h1>
+            <h1>Ahmed</h1>
+            <h1>Ahmed</h1>
+            <h1>Ahmed</h1>
+        </div>
+    )
+}
+function Raza() {
+    return (
+        <div>
+            <h1>Raza</h1>
+            <h1>Raza</h1>
+            <h1>Raza</h1>
+            <h1>Raza</h1>
+            <h1>Raza</h1>
+        </div>
+    )
 }
 
-export default App;
+export default function App() {
+    const data = [
+        { gender: "Male" },
+        { gender: "feMale" },
+        { gender: "kids" },
+        { gender: "boys" },
+    ]
+
+
+    // head = "Full Stack Developer"
+    let [count, setCount] = useState(1)
+    // const [dataa, setDataa] = useState({gender:'Male',name:'Ahmed'})
+    // const [color, setColor] = useState('green')
+    return (
+        <div>
+            {count == 1 && (
+                <Ahmed />
+            )
+            }
+            {count == 2 && (
+                <Raza />
+            )}
+            <button onClick={() => setCount(1)}>first</button>
+            <button onClick={() => setCount(2)}>second</button>
+        </div>
+    )
+}
